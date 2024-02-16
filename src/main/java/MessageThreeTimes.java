ad5b90de13d4b54383bc1046bc7da6357af24067
Daniil Starov <jazzzydan@hotmail.com>
2024-02-16 11:35:11 +0200

    Initial commit

---
+++ src/main/java/MessageThreeTimes.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class MessageThreeTimes {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write a message:");
+
+        //Read the user's string input, save it to program memory
+        //"String message = (user input)"
+        String message = scanner.nextLine();
+
+        // Print the user input twice
+        System.out.println(message);
+        System.out.println(message);
+        System.out.println(message);
+    }
+}
